73730d93f166838919acd4365d6e26e6cf662b17
James Kane <james.kane59@outlook.com>
2022-12-08 21:06:17 +0000

    remove projects from project links list and projects array

--- src/nav.js
+++ src/nav.js
@@ -8,6 +8,7 @@ import logo from '../assets/logo.svg';
 import addTask from '../assets/add-task.svg';
 import { renderAllTasks, renderTodaysTasks, renderWeeksTasks, renderTasksByTag } from './task-display';
 import { renderAddTaskModal } from './add-task-modal';
+import { tasks, projects } from './task';
 
 const navLinks = Array.from(document.querySelectorAll('.navigation'));
 const projectLinks = document.querySelector('.project-links');
@@ -129,5 +130,11 @@ const _removeButton = () => {
 }
 
 const removeProject = (e) => {
-	return 
+	let _projectName = e.target.parentNode.firstChild.nextSibling.innerText;
+	let _project = projects.find(project => project == _projectName);
+	let _projectIdx = projects.indexOf(_project);
+
+	document.getElementById(`${_project}`).remove();
+	if (_projectIdx != -1) projects.splice(_projectIdx, 1);
+	console.log(projects);
 }
